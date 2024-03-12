6b1ef149601bd005878ddc0c4ef905355083e15e
Sebastien <allouchesebastien@gmail.com>
2024-03-12 14:20:25 +0100

    Config

--- src/main/java/org/workshop/coffee/repository/OldSearchRepository.java
+++ src/main/java/org/workshop/coffee/repository/OldSearchRepository.java
@@ -19,9 +19,16 @@ public class OldSearchRepository {
     DataSource dataSource;
 
     public List<Product> searchProduct (String input) {
+         // lowercase the input
         var lowerInput = input.toLowerCase(Locale.ROOT);
+        
+        // create a query matching the input to the product name or the product description
         var query = em.createNativeQuery("Select * from Product where lower(description) like '%" + lowerInput + "%' OR lower(product_name) like '%" + lowerInput + "%'", Product.class);
+
+        // execute the query
         var resultList = (List<Product>) query.getResultList();
+
+        // return the list of results
         return resultList;
     }
 
