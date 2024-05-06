5bdc59e1b48a6c2a3bb224c211a0050792050a70
Brian Vermeer <brianvermeer@snyk.io>
2024-05-06 14:27:51 +0200

    search product FINISHED.... get some beers!!!

--- src/main/java/org/workshop/coffee/repository/SearchRepository.java
+++ src/main/java/org/workshop/coffee/repository/SearchRepository.java
@@ -19,7 +19,12 @@ public class SearchRepository {
     DataSource dataSource;
 
     public List<Product> searchProduct (String input) {
-        return null;
+        //search for products by description or product name
+        //search is case insensitive
+        var lowerInput = input.toLowerCase(Locale.ROOT);
+        String query = "Select * from Product where lower(description) like '%" + lowerInput + "%' OR lower(product_name) like '%" + lowerInput + "%'";
+        var resultList = (List<Product>) em.createNativeQuery(query, Product.class).getResultList();
+        return resultList;
     }
 
 }
