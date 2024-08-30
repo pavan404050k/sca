d29f05e7f9fab079a08fa4f8c8cb83f78eb489ae
Urszula Etheridge <urszulaetheridge@j5rh475d2l.cust.communityfibre.co.uk>
2024-08-30 11:50:36 +0100

    product search

--- src/main/java/org/workshop/coffee/repository/SearchRepository.java
+++ src/main/java/org/workshop/coffee/repository/SearchRepository.java
@@ -19,7 +19,19 @@ public class SearchRepository {
     DataSource dataSource;
 
     public List<Product> searchProduct (String input) {
-        return null;
+             // lowercase the input
+        var lowerInput = input.toLowerCase(Locale.ROOT);
+
+        // create a query matching the input to the product name or the product description
+        var query = em.createNativeQuery("Select * from Product where lower(description) like '%" + lowerInput + "%' OR lower(product_name) like '%" + lowerInput + "%'", Product.class);
+
+        // execute the query
+        var resultList = (List<Product>) query.getResultList();
+
+        // return the list of results
+        return resultList;
+}
+
     }
 
-}
+
