319640da9afc4d74dafefe9d3f865bfe04d502b3
Brian Vermeer <brianvermeer@snyk.io>
2024-11-07 16:07:36 +0100

    implementing search of products

--- src/main/java/org/workshop/coffee/repository/SearchRepository.java
+++ src/main/java/org/workshop/coffee/repository/SearchRepository.java
@@ -19,7 +19,11 @@ public class SearchRepository {
     DataSource dataSource;
 
     public List<Product> searchProduct (String input) {
-        return null;
+        //search for products by description or product name
+        var lowerInput = input.toLowerCase(Locale.ROOT);
+        String query = "Select * from Product where lower(description) like '%" + lowerInput + "%' OR lower(product_name) like '%" + lowerInput + "%'";
+        var resultList = (List<Product>) em.createNativeQuery(query, Product.class).getResultList();
+        return resultList;
     }
 
 }
